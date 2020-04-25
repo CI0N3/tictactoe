5922ebae622b9980f4fac93a4a3797739a50ede2
Rahel Joshi <RahelJoshi@gmail.com>
2020-04-25 18:15:02 -0500

    tic tac toe ai with minimax

---
+++ tictactoe.py
@@ -0,0 +1,241 @@
+"""
+Tic Tac Toe Player
+"""
+
+import math, random
+
+X = "X"
+O = "O"
+EMPTY = None
+
+
+def initial_state():
+    """
+    Returns starting state of the board.
+    """
+    return [[EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]]
+
+def player(board):
+    x = 0
+    o = 0
+    for letter in str(board):
+        if letter == X:
+            x += 1
+        if letter == O:
+            o += 1
+    if x < o or (x == o):
+        return X
+    elif o < x:
+        return O
+    """
+    Returns player who has the next turn on a board.
+    """
+    raise NotImplementedError
+
+def actions(board):
+    list = []
+    for i in range (3):
+        for j in range (3):
+            if board[i][j] == EMPTY:
+                list += {(i, j)}
+
+    return list
+
+    """
+    Returns set of all possible actions (i, j) available on the board.
+    """
+    raise NotImplementedError
+
+
+def result(board, action):
+    ij = ""
+    action = str(str(action).split())
+    for number in action:
+        if number == "0" or number == "1" or number == "2":
+            ij += number
+
+    i = int(ij[0])
+    j = int(ij[1])
+
+    score_1 = board[0][0]
+    score_2 = board[0][1]
+    score_3 = board[0][2]
+    score_4 = board[1][0]
+    score_5 = board[1][1]
+    score_6 = board[1][2]
+    score_7 = board[2][0]
+    score_8 = board[2][1]
+    score_9 = board[2][2]
+
+    if i == 0 and j == 0:
+        score_1 = player(board)
+    if i == 0 and j == 1:
+        score_2 = player(board)
+    if i == 0 and j == 2:
+        score_3 = player(board)
+    if i == 1 and j == 0:
+        score_4 = player(board)
+    if i == 1 and j == 1:
+        score_5 = player(board)
+    if i == 1 and j == 2:
+        score_6 = player(board)
+    if i == 2 and j == 0:
+        score_7 = player(board)
+    if i == 2 and j == 1:
+        score_8 = player(board)
+    if i == 2 and j == 2:
+        score_9 = player(board)
+
+    return [[score_1, score_2, score_3],
+            [score_4, score_5, score_6],
+            [score_7, score_8, score_9]]
+    """
+    Returns the board that results from making move (i, j) on the board.
+    """
+    raise NotImplementedError
+
+def winner(board):
+    for i in range(3):
+        if board[i][0] == board[i][1] and board[i][0] == board[i][2] and board[i][0] == X:  # Checks horizontally
+            return X
+        if board[i][0] == board[i][1] and board[i][0] == board[i][2] and board[i][0] == O:  # Checks horizontally
+            return O
+    for i in range(3):
+        if board[0][i] == board[1][i] and board[0][i] == board[2][i] and board[0][i] == X:  # Checks vertically
+            return X
+        if board[0][i] == board[1][i] and board[0][i] == board[2][i] and board[0][i] == O:  # Checks vertically
+            return O
+    if (board[0][0] == board[1][1] and board[0][0] == board[2][2] or board[0][2] == board[1][1] and board[0][2] == board[2][0]) and board[1][1] == X:  # Checks diagonally
+        return X
+    if (board[0][0] == board[1][1] and board[0][0] == board[2][2] or board[0][2] == board[1][1] and board[0][2] == board[2][0]) and board[1][1] == O:  # Checks diagonally
+        return O
+    return None
+
+    """
+    Returns the winner of the game, if there is one.
+    """
+    raise NotImplementedError
+
+
+def terminal(board):
+    for i in range(3):
+        if board[i][0] == board[i][1] and board[i][0] == board[i][2] and board[i][0] != None:   #Checks horizontally
+            return True
+    for i in range(3):
+        if board[0][i] == board[1][i] and board[0][i] == board[2][i] and board[0][i] != None:   #Checks vertically
+            return True
+    if (board[0][0] == board[1][1] and board[0][0] == board[2][2] or board[0][2] == board[1][1] and board[0][2] == board[2][0]) and board[1][1] != None:  #Checks diagonally
+        return True
+    if board[0][0] != EMPTY and board[0][1] != EMPTY and board[0][2] != EMPTY and board[1][0] != EMPTY and board[1][1] != EMPTY and board[1][2] != EMPTY and board[2][0] != EMPTY and board[2][1] != EMPTY and board[2][2] != EMPTY:
+        return True
+    return False
+    """
+    Returns True if game is over, False otherwise.
+    """
+    raise NotImplementedError
+
+
+def utility(board):
+    if winner(board) == X:
+        return 1
+    elif winner(board) == O:
+        return -1
+    else:
+        return 0
+    """
+    Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
+    """
+    raise NotImplementedError
+
+board = [[EMPTY, X, O],
+        [O, X, X],
+        [X, EMPTY, O]]
+def minimax(board):
+
+    def maxvalue(board):
+        if terminal(board):
+            return utility(board)
+        value = -math.inf
+        for action in actions(board):
+            value = max(value, minvalue(result(board, action)))
+        return value
+
+    def minvalue(board):
+        if terminal(board):
+            return utility(board)
+        value = math.inf
+        for action in actions(board):
+            value = min(value, maxvalue(result(board, action)))
+        return value
+
+    print (maxvalue(board))
+    print (minvalue(board))
+
+    if terminal(board):
+        return None
+    if player(board) == X:
+        for action in actions(board):
+            if utility(result(board, action)) == minvalue(board):
+                return action
+
+        print("fail")
+        return actions(board)
+
+
+    if player(board) == O:
+        for action in actions(board):
+            if utility(result(board, action)) == maxvalue(board):
+                return action
+
+        print("fail")
+        return actions(board)
+
+        """""
+        for action in actions(board):
+            board_1 = result(board, action)  # all board possibilities for O
+            #print(board_1)
+            i = 0
+            if winner(board_1) == O:
+                return action
+            for action_1 in actions(board_1):  # all board possibilities 1 move ahead for X
+                board_2 = result(board_1, action_1)
+                if winner(board_2) == X:
+                    return action_1
+        for action in actions(board):
+            board_1 = result(board, action)
+            for action_1 in actions(board_1):  # all board possibilities 1 move ahead for X
+                board_2 = result(board_1, action_1)
+                for action_2 in actions(board_2):  # we looking for board_5 (act. num 4 tho)
+                    board_3 = result(board_2, action_2)
+                    for action_3 in actions(board_3):
+                        board_4 = result(board_3, action_3)
+                        for action_4 in actions(board_4):
+                            board_5 = result(board_4, action_4)  #maybe look for all combos of board but -1 for action that has a loss
+                            for action_5 in actions(board_5):
+                                board_6 = result(board_5, action_5)
+                                if winner(board_6) == X:
+                                    i += 1
+                                    if i > 1300:
+                                        print("hello")
+                                        return action_5
+                                        
+                                        """""
+
+
+
+            #for action_2 in actions(board_2):
+                #board_3 = result(board_2, action_2)
+                #if winner(board_3) == O:
+                    #return action_2
+        #print("random")
+        #return actions(board)[random.randint(0, len(actions(board))-1)]
+
+
+    """
+    Returns the optimal action for the current player on the board.
+    """
+    raise NotImplementedError
+
+print(minimax(board))
